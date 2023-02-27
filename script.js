bb61837edff991aff86eef60295eb951d73a1d81
AntonKurenov <anton.kurenov@yandex.ru>
2023-02-27 23:33:13 +0300

    Added feedback button and network error message and media in css

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const yesButton = document.querySelector('.button-yes');
 const radioOptions = document.querySelector('.radio-group');
 const enterButton = document.querySelector('.button-enter');
 const jokeText = document.querySelector('.text');
+const feedbackButtons = document.querySelector('.feedback');
+const voteUpButton = document.querySelector('.vote--up');
+const voteDownButton = document.querySelector('.vote--down');
 const url = 'https://v2.jokeapi.dev/joke/any';
 let isDark = true;
 
 function radioButtonsHandler(event) {
-	// console.log('hellr')
 	let target = event.target;
 	if (target.tagName !== 'INPUT') {
 		return;
@@ -17,43 +19,64 @@ function radioButtonsHandler(event) {
 	} else {
 		isDark = false;
 	}
-	console.log(enterButton.classList);
 	if (!enterButton.classList.contains('show')) {
 		enterButton.classList.add('show');
-		// console.log(enterButton.className);
 	}
-	// fetchJoke();
 }
 
 function placeJoke(data) {
-	// jokeText.style.display = 'block';
 	if (data.type === 'twopart') {
-		console.log('data.setup = ', data.setup.split());
-		let time = data.setup.split(' ').length * 260;
+		let time = data.setup.split(' ').length * 190;
 		console.log('time = ', time);
 		jokeText.innerText = `${data.setup}\n.....`;
 		setTimeout(() => jokeText.innerText = `${data.setup}\n${data.delivery}`, time);
 	} else {
 		jokeText.innerText = data.joke;
 	}
+	setTimeout(() => enterButton.innerText = 'Next joke >>', 700);
+	setTimeout(() => feedbackButtons.classList.toggle('show'), 1300);
+}
+
+function placeError(error) {
+	jokeText.innerText = `Sorry, an error occurred, please try again later.\n` +
+		`More about that error in console.`;
+	console.log(error);
 }
 
 function fetchJoke() {
+	if (feedbackButtons.classList.contains('show')) {
+		feedbackButtons.classList.toggle('show');
+	}
 	let addr = url;
 	if (!isDark) {
 		addr += '?safe-mode';
 	}
 	console.log('style = ', jokeText.style.display)
 	if (!jokeText.classList.contains('show')) {
-		console.log('hhhh');
 		jokeText.classList.add('show');
 	}
 	jokeText.innerText = 'Your joke is loading...';
 	fetch(addr)
-		.then((response) => response.json())
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error('Error occured!');
+			}
+			return response.json();
+		})
 		.then((data) => placeJoke(data))
-		.catch((error) => console.log(error));
+		.catch((error) => placeError(error));
+}
 
+function feedbackHandler(event) {
+	let ev = event.target;
+	if (event.target.tagName === 'BUTTON') {
+		event.target.classList.toggle('pressed');
+		return;
+	}
+	if (event.target.tagName === 'SPAN') {
+		event.target.parentElement.classList.toggle('pressed');
+		return;
+	}
 }
 
 enterButton.addEventListener('click', fetchJoke);
@@ -64,3 +87,5 @@ yesButton.addEventListener("click", (event) => {
 	explicitOptions.style.display = 'flex';
 	yesButton.style.backgroundColor = 'green';
 })
+
+feedbackButtons.addEventListener('click', feedbackHandler);
